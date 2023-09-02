fda813acae5d0fec9e8cd9a885d7d7f27e93686e
carlamraphel <carlamraphel3@gmail.com>
2023-09-02 09:00:48 +0800

    Deleted backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket          = "final-project-bucket1"
-    key             = "terraform.tfstate"
-    region          = "ap-southeast-1"
-    dynamodb_table  = "tfstate-table"
-  }
-}
